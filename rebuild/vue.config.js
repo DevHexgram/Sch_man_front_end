b04d4f467cc7fc585e9e2a8bb95bc0034d2ffbbc
legends-killer <958775729@qq.com>
2020-01-22 02:23:07 +0800

    add: vue-markdown-loader

--- rebuild/vue.config.js
+++ rebuild/vue.config.js
@@ -2,6 +2,17 @@ module.exports = {
     css: {
         extract: false
     },
-
+    chainWebpack: config => {
+        config.module.rule('md')
+          .test(/\.md/)
+          .use('vue-loader')
+          .loader('vue-loader')
+          .end()
+          .use('vue-markdown-loader')
+          .loader('vue-markdown-loader/lib/markdown-compiler')
+          .options({
+            raw: true
+          })
+      },
     publicPath: './'
 }

bc76f643004a5fbc4a4a84400b178e5c4896ca3c
legends-killer <958775729@qq.com>
2020-01-25 03:47:30 +0800

    add: version details

---
+++ rebuild/src/views/Version.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="markdown-body">
+      <V />
+  </div>
+</template>
+
+<script>
+import "highlight.js/styles/github.css";
+import V from "./Version.md";
+import "github-markdown-css";
+export default {
+  name: "about",
+  components: {
+      V
+  },
+  data() {},
+  methods: {}
+};
+</script>

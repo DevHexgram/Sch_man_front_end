40d0d5643d02dc84054d7e3644e6a34be8b1446f
legends-killer <958775729@qq.com>
2020-01-25 03:46:07 +0800

    add: about page

---
+++ rebuild/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="markdown-body">
+      <MD />
+  </div>
+</template>
+
+<script>
+import "highlight.js/styles/github.css";
+import MD from "./About.md";
+import "github-markdown-css";
+export default {
+  name: "about",
+  components: {
+      MD
+  },
+  data() {},
+  methods: {}
+};
+</script>

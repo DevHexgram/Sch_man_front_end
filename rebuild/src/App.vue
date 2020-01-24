ab1e9d0f55f039b5950a1ee3370417bb912c6b07
legends-killer <958775729@qq.com>
2020-01-25 03:44:25 +0800

    add: button bar

--- rebuild/src/App.vue
+++ rebuild/src/App.vue
@@ -1,18 +1,85 @@
 <template>
-  <div class="outter" >
-    <router-view/>
+  <div class="outter box" :style="{ background: backGround , 'background-size': 'cover'}">
+    <router-view style="content" />
+    <div v-show="isLogin"><changeBG /></div>
+    <div v-show="isLogin" class="footer">
+      <div class="nav" v-on:click="goToAffair"><img src="./assets/affair.png" ></div>
+      <div class="nav" v-on:click="goToMore"><img src="./assets/more.png" ></div>
+      <div class="nav" v-on:click="goToSet"><img src="./assets/setting.jpg" ></div>
+    </div>
   </div>
 </template>
 
 <style>
-  .outter {
-    margin-top: 0px;
-    width: 100%;
-    height: 100%;
-    padding: 0px;
-
-  }
+img {
+  width: 50%;
+  height: 100%;
+}
+.outter {
+  margin: 0px;
+  width: 100%;
+  height: 100%;
+  padding: 0px;
+}
+.box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh; 
+}
+.content {
+  flex: 1;
+}
+.footer {
+  width: 100%;
+  height: 50px;
+  max-height: 50px;
+  display: flex;
+  flex-direction: row;
+  padding: 0px;
+  margin: 0px;
+  background: #fff;
+}
+.nav {
+  flex: 1; /*等分*/
+  text-align: center;
+  background: inherit;
+  padding: 0px;
+  margin: 0px;
+}
 </style>
 
 <script>
+import changeBG from "@/components/ChangeBG";
+export default {
+  components: {
+    changeBG,
+  },
+  data() {
+    return {
+      isLogin: false,
+      backGround: "url(http://www.sweetbeecr.com:12210/backgroundImage)"
+    };
+  },
+  async created() {
+    const token = localStorage.getItem("token");
+    if (token == null) this.isLogin = false;
+    else this.isLogin = true;
+  },
+  methods: {
+    goToAffair() {
+      this.$router.push('/home')
+      window.location.reload()
+    },
+    goToMore() {
+      this.$router.push('/more')
+      window.location.reload()
+    },
+    goToSet() {
+      this.$router.push('/setting')
+      window.location.reload()
+    },
+  }
+};
 </script>
